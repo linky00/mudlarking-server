cca9e6b78e7584cb7c3354436fa1a8c99163fcf0
Ada Null <ada@worldenddisk.com>
2024-07-31 02:49:40 +0100

    calculating font width and making shore based on it

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,45 @@ use axum::{
     Json,
     Router,
 };
-use std::env;
+use std::{
+    env,
+    sync::Arc
+};
 use tower_http::cors::CorsLayer;
+use crate::font::*;
 use crate::shore::*;
 use crate::text_table::*;
 
 mod shore;
 mod text_table;
+mod font;
+
+struct AppState<'a> {
+    font_sizer: FontSizer<'a>,
+    text_table: TextTable,
+}
 
 #[tokio::main]
 async fn main() {
+    let font_sizer = font::FontSizer::new(include_bytes!("EBGaramond-Regular.ttf"), 0);
+
     let corpus_json = include_str!("corpus.json");
     let text_table = TextTable::from_json(&corpus_json);
+
+    let app_state = AppState { font_sizer, text_table };
 
     let port = env::var("PORT").expect("'PORT' env should be set");
 
     let app = Router::new()
         .route("/", get(make_shore))
-        .with_state(text_table)
+        .with_state(Arc::new(app_state))
         .layer(CorsLayer::permissive());
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.expect(&format!("Can bind to port {port}"));
     axum::serve(listener, app).await.expect("Should be able to start server");
 }
 
-async fn make_shore(State(text_table): State<TextTable>) -> Json<Shore> {
-    let shore = Shore::new(20, 200, text_table);
-    println!("{shore}");
+async fn make_shore<'a>(State(app_state): State<Arc<AppState<'a>>>) -> Json<Shore> {
+    let shore = Shore::new(950.0, 200, &app_state.text_table, &app_state.font_sizer);
     Json(shore)
 }
--- src/shore.rs
+++ src/shore.rs
@@ -2,62 +2,65 @@ use std::fmt;
 use serde::Serialize;
 use uuid::Uuid;
 use crate::TextTable;
+use crate::FontSizer;
 
 #[derive(Serialize)]
 pub struct Shore {
-    width: u16,
-    height: u16,
     contents: Vec<Item>,
 }
 
 impl Shore {
-    pub fn new(width: u16, height: u16, text_table: TextTable) -> Shore {
+    pub fn new(width: f32, lines: u16, text_table: &TextTable, font_sizer: &FontSizer) -> Shore {
         Shore {
-            width,
-            height,
-            contents: Self::generate_contents(width, height, text_table),
+            contents: Self::generate_contents(width, lines, text_table, font_sizer),
         }
     }
 
-    fn generate_contents(width: u16, height: u16, text_table: TextTable) -> Vec<Item> {
+    fn generate_contents(width: f32, lines: u16, text_table: &TextTable, font_sizer: &FontSizer) -> Vec<Item> {
         let mut contents = Vec::new();
 
-        for _ in 0..(width * height) {
-            let text = text_table.get_text();
-            contents.push(Item::new(text));
+        for n in 0..(lines) {
+            let mut current_width = 0.0;
+            while current_width < width {
+                let text = text_table.get_text();
+                let new_width = font_sizer.get_width(&text, 16.0);
+                contents.push(Item::new(text, n, current_width));
+                current_width += new_width;
+            };
         }
 
         contents
     }
-
-    fn get(&self, x: u16, y: u16) -> &Item {
-        &self.contents[(self.width * y + x) as usize]
-    }
 }
 
-impl fmt::Display for Shore {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut out = String::new();
-        for y in 0..self.height {
-            for x in 0..self.width {
-                out = format!("{} {}", out, self.get(x, y).text);
-            }
-            out += "\n";
-        }
-        write!(f, "{out}")
-    }
-}
+// impl fmt::Display for Shore {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         let mut out = String::new();
+//         for y in 0..self.height {
+//             for x in 0..self.width {
+//                 out = format!("{} {}", out, self.get(x, y).text);
+//             }
+//             out += "\n";
+//         }
+//         write!(f, "{out}")
+//     }
+// }
 
 #[derive(Clone, Serialize)]
-struct Item {
+pub struct Item {
     text: String,
+    line: u16,
+    offset: f32,
     id: Uuid,
+    
 }
 
 impl Item {
-    pub fn new(text: String) -> Item {
+    pub fn new(text: String, line: u16, offset: f32) -> Item {
         Item { 
             text,
+            line,
+            offset,
             id: Uuid::new_v4(),
         }
     }
